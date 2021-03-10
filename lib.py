2c4b6080fb62d5e9d76d017308ee76408cd7d8cd
pierre-si <62605527+pierre-si@users.noreply.github.com>
2021-03-10 01:09:55 +0100

    multiheadattention

--- lib.py
+++ lib.py
@@ -24,3 +24,53 @@ class PositionalEncoding(nn.Module):
     def forward(self, x):
         batch_size, seq_len = x.size()
         return self.encoding[:seq_len, :]
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_model, n_head):
+        super().__init__()
+        self.n_head = n_head
+        self.attention = ScaleDotProductAttention()
+        # the projections for the different heads are done with the same matrix
+        # they are then split and the attention is applied
+        self.w_q = nn.Linear(d_model, d_model)
+        self.w_k = nn.Linear(d_model, d_model)
+        self.w_v = nn.Linear(d_model, d_model)
+        # matrix for the final projection after concaneting the attention outputs
+        self.w_concat = nn.Linear(d_model, d_model)
+    
+    def forward(self, q, k, v, mask=None):
+        q, k, v = self.w_q(q), self.w_k(k), self.w_v(v)
+        q, k, v = self.split(q), self.split(k), self.split(v)
+
+        out, attention = self.attention(q, k, v, mask=mask)
+
+        out = self.concat(out)
+        out = self.w_concat(out)
+
+        return out
+
+    def split(self, tensor):
+        """split along d_model by number of heads 
+        :param tensor: [batch_size, length, d_model]
+        :return: [batch_size, n_head, length, d_model/n_head]
+        """
+
+        batch_size, length, d_model = tensor.size()
+        d_tensor =  d_model // self.n_head
+
+        tensor = tensor.view(batch_size, self.n_head, length, d_tensor)
+
+        return tensor
+
+    def concat(self, tensor):
+        """inverse function of self.split(tensor: torch.Tensor)
+
+        :param tensor: [batch_size, n_head, length, d_model/n_head]
+        :return: [batch_size, length, d_model]
+        """
+
+        batch_size, n_head, length, d_tensor = tensor.size()
+        d_model = n_head * d_tensor
+
+        return tensor.view(batch_size, length, d_model)
+
